9256c8c06962476c6c78f4f807683f0542b1f187
Kumar Chandrakant <kumchand0@publicisgroupe.net>
2018-11-26 14:30:31 +0530

    Updated files as par the review comments on the tututorial.

--- core-java/src/main/java/com/baeldung/encoding/CharacterEncodingExamples.java
+++ core-java/src/main/java/com/baeldung/encoding/CharacterEncodingExamples.java
@@ -1,32 +1,35 @@
 package com.baeldung.encoding;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CharacterEncodingExamples {
 
-    static String readFile(String filePath, String encoding) throws IOException {
-        File file = new File(filePath);
-        StringBuffer buffer = new StringBuffer();
-        try (InputStreamReader isr = new InputStreamReader(new FileInputStream(file), encoding)) {
-            int data;
-            while ((data = isr.read()) != -1) {
-                buffer.append((char) data);
-            }
-        }
-        return buffer.toString();
+    static String decodeText(String input, String encoding) throws IOException {
+        return 
+            new BufferedReader(
+                new InputStreamReader(
+                    new ByteArrayInputStream(
+                        input.getBytes()), 
+                    Charset.forName(encoding)))
+            .readLine();
     }
 
     static String convertToBinary(String input, String encoding) throws UnsupportedEncodingException {
-        byte[] bytes = input.getBytes(encoding);
-        StringBuffer buffer = new StringBuffer();
-        for (int b : bytes) {
-            buffer.append(Integer.toBinaryString((b + 256) % 256));
-            buffer.append(" ");
-        }
-        return buffer.toString();
+        byte[] encoded_input = Charset.forName(encoding)
+            .encode(input)
+            .array();  
+        return
+            IntStream.range(0, encoded_input.length)
+            .map(i -> encoded_input[i])
+            .mapToObj(e -> Integer.toBinaryString((e + 256) % 256))
+            .map(e -> String.format("%1$" + Byte.SIZE + "s", e).replace(" ", "0"))
+            .collect(Collectors.joining(" "));
     }
 }
